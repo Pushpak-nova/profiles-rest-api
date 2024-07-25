133f8ba814b0cdbbcc115761ed236a1286cacacb
RajHota <evi.pushpak@gmail.com>
2024-07-25 19:43:52 +0530

    Added hello API View

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -5,34 +5,35 @@ from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 
-class UserProfileManager (BaseUserManager):
-    """Manager for User Profiles"""
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
-        """Create a new User Profile"""
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User Must have a email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-        def create_superuser(self, email, name, password):
-            """create a new super user"""
-            user = self.create_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using = self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
 
-class UserProfile (AbstractBaseUser, PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
@@ -42,16 +43,16 @@ class UserProfile (AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrieve Full Name of User"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """Retrieve short name of User"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """Return String Representation of User"""
+        """Return string representation of user"""
         return self.email
